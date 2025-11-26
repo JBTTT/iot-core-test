675bf6cab61c953afa3a1afe6f25c56a6b09f867
jibin <perseverancejbt@hotmail.com>
2025-11-26 17:58:53 +0800

    IOT Core test

---
+++ bootstrap/main.tf
@@ -0,0 +1,63 @@
+###############################################
+# BOOTSTRAP MODULE
+# Creates S3 bucket + DynamoDB lock table
+# for Terraform remote backend
+###############################################
+
+provider "aws" {
+  region = var.region
+}
+
+###############################################
+# S3 STATE BUCKET
+###############################################
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.state_bucket_name
+
+  tags = {
+    Name    = var.state_bucket_name
+    Project = var.prefix
+    Environment = "bootstrap"
+  }
+}
+
+# Block all public access (required for Terraform state)
+resource "aws_s3_bucket_public_access_block" "state_bucket" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
+
+# Versioning strongly recommended for Terraform state
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+###############################################
+# DynamoDB Lock Table for Terraform
+###############################################
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = var.lock_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name       = var.lock_table_name
+    Project    = var.prefix
+    Environment = "bootstrap"
+  }
+}
