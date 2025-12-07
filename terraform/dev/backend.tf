a8daef5b44adac77e5e87db30464d9bdfbc8e5a6
jibin <perseverancejbt@hotmail.com>
2025-12-08 01:27:03 +0800

    Add lambda.zip for alert handler

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,8 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "cet11-grp1-terraform-state"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "cet11-grp1-iot-terraform-lock"
+    bucket        = "cet11-grp1-terraform-state"
+    key           = "dev/terraform.tfstate"
+    region        = "us-east-1"
+
+    # New Terraform backend locking method
+    use_lockfile  = true
+
+    encrypt       = true
   }
 }
