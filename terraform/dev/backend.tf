e6f3a341dfa508e58ed0e0c376bd11744f352736
JBTTT <133977410+JBTTT@users.noreply.github.com>
2025-12-08 06:26:23 +0000

    Auto-format Terraform files

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -5,5 +5,13 @@ terraform {
     region       = "us-east-1"
     use_lockfile = true
     encrypt      = true
+    bucket = "cet11-grp1-terraform-state"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+
+    # New Terraform backend locking method
+    use_lockfile = true
+
+    encrypt = true
   }
 }
