59e07d6e44a3aa206a7fe69740b737d515fde385
jibin <perseverancejbt@hotmail.com>
2025-12-08 14:40:12 +0800

    Script threshold change

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,17 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "cet11-grp1-terraform-state"
-    key          = "dev/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
-    bucket = "cet11-grp1-terraform-state"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-
-    # New Terraform backend locking method
-    use_lockfile = true
-
-    encrypt = true
+    bucket        = "cet11-grp1-terraform-state"
+    key           = "dev/terraform.tfstate"
+    region        = "us-east-1"
+    use_lockfile  = true
+    encrypt       = true
   }
 }
