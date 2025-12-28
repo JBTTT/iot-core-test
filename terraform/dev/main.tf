2680050601a5b255ecda3740e71320baf4cabaa8
JBTTT <133977410+JBTTT@users.noreply.github.com>
2025-12-28 09:49:19 +0000

    chore(terraform): auto-format

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -137,9 +137,9 @@ module "monitoring" {
   env    = var.env
   region = var.region
 
-  vpc_id              = module.vpc.vpc_id
-  public_subnet_ids   = module.vpc.public_subnet_ids
-  private_subnet_ids  = module.vpc.private_subnet_ids
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_subnet_ids
 
   allowed_cidrs = ["YOUR_IP/32"]
 }
