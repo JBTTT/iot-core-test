da5eb91438d24f6456eacadc6ec28073de2bf5af
JBTTT <133977410+JBTTT@users.noreply.github.com>
2025-12-28 16:07:08 +0000

    chore(terraform): auto-format

--- terraform/modules/iot_simulator_ecs/main.tf
+++ terraform/modules/iot_simulator_ecs/main.tf
@@ -42,11 +42,11 @@ resource "aws_ecs_service" "this" {
   desired_count   = 1
   launch_type     = "FARGATE"
 
-network_configuration {
-  subnets          = var.subnet_ids
-  security_groups  = var.security_group_ids
-  assign_public_ip = false
-}
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = var.security_group_ids
+    assign_public_ip = false
+  }
 
   deployment_minimum_healthy_percent = 50
   deployment_maximum_percent         = 200
